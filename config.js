930f876f89b87e63024aade0eb9e813a831b0663
Lucas Turpin <lucas_turpin@hotmail.com>
2018-09-22 13:45:22 -0400

    Initialize CircleCI config and db-migrate #8 #9
    Incomplete config for CircleCI
    Added database configuration for db-migrate
    Added sample migration
    Missing implementation to hide user database credentials in hidden.js

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {
+    db: {
+        host: '127.0.0.1',
+        user: 'dbuser',
+        password: 'very-secret',
+        db: 'anansi_db'
+    }
+};
+
+// TODO: import hidden.js and extract credentials
+module.exports = config;
