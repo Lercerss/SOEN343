73e9831d6e9e9306b298df432e536d1dcba5c527
Alvyn <alvyn279@gmail.com>
2018-09-26 20:35:48 -0400

    Added migrations file for User model #10
    - User table is structured with necessary fields

---
+++ migrations/20180927003148-users.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+    dbm = options.dbmigrate;
+    type = dbm.dataType;
+    seed = seedLink;
+};
+
+exports.up = function(db) {
+    return db.createTable('user',
+        {
+            client_id: { type: 'int', primaryKey: true, autoIncrement: true },
+            username: 'string',
+            password: 'string',
+            firstName: 'string',
+            lastName: 'string',
+            email: 'string',
+            address: 'string',
+            phoneNumber: 'string',
+            isAdmin: 'boolean',
+            timestamp: 'datetime',
+        }
+    ).then(
+      function(result) {
+        db.dropTable('test');
+      },
+      function(err) {
+        return;
+      }
+    );
+};
+
+exports.down = function(db) {
+    return db.dropTable('user');
+};
+
+exports._meta = {
+    "version": 1
+};
