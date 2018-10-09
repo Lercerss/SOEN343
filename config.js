83cd74cad0ccb5aa0e3c42924557395c34c2dcff
chimano <chiracmanoukian@hotmail.com>
2018-10-08 20:04:33 -0400

    Set up a test database #30
    - Set up test database migration command
    - Updated config to get database name off env vars

--- config.js
+++ config.js
@@ -1,14 +1,21 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 try {
     dotenv.config();
 } catch (ex) {}
 
+let dbName = "";
+if (process.env.NODE_ENV == "test") {
+    dbName = process.env.TEST_DATABASE;
+} else {
+    dbName = process.env.DATABASE_NAME;
+}
+
 export const config = {
     db: {
-        host: '127.0.0.1', // localhost
-        user: 'dbuser',
+        host: "127.0.0.1", // localhost
+        user: "dbuser",
         password: process.env.MYSQL_PASSWORD,
-        database: 'anansi_db'
+        database: dbName
     }
 };
