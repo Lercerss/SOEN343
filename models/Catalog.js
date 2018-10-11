e93dd3575e05125acb780ac1ee27c9369a15072f
pvacca97 <patrick.vacca@hotmail.com>
2018-10-11 19:08:53 -0400

    Issue #27
    - Added functionality to edit media item
    - Fixed typos
    - rebased
    - fxed eslint issues

--- models/Catalog.js
+++ models/Catalog.js
@@ -3,17 +3,16 @@ import { Magazine } from './Magazine';
 import { Movie } from './Movie';
 import { Music } from './Music';
 
-var id = 0;
 var mediaList = new Array();
 
 export class Catalog {
-    addItem(type, fields, callback) {
+    static addItem(type, fields, callback) {
         console.log(fields);
         if (type === 'Book'){
             var bookDict = {
                 title: fields.title,
                 author: fields.author,
-                format: fields.author,
+                format: fields.format,
                 pages: fields.pages,
                 publisher: fields.publisher,
                 language: fields.language,
@@ -60,34 +59,97 @@ export class Catalog {
             var movie = new Movie(movieDict);
             mediaList.push(movie);
             callback();
+        } else {
+            var err = new Error('Error in type specified.');
+            callback(err);
         }
     }
 
-    static editem(type, fields) {}
+    static editItem(type, fields, index, callback) {
+        console.log(fields);
+        if (type === 'Book'){
+            var bookDict = {
+                title: fields.title,
+                author: fields.author,
+                format: fields.format,
+                pages: fields.pages,
+                publisher: fields.publisher,
+                language: fields.language,
+                isbn10: fields.isbn10,
+                isbn13: fields.isbn13
+            };
+            var book = new Book(bookDict);
+            mediaList[index] = book;
+            callback();
+        } else if (type === 'Magazine'){
+            var magazineDict = {
+                title: fields.title,
+                publisher: fields.publisher,
+                language: fields.language,
+                isbn10: fields.isbn10,
+                isbn13: fields.isbn13
+            };
+            var magazine = new Magazine(magazineDict);
+            mediaList[index] = magazine;
+            callback();
+        } else if (type === 'Music'){
+            var musicDict = {
+                title: fields.title,
+                artist: fields.artist,
+                label: fields.label,
+                releaseDate: fields.releaseDate,
+                asin: fields.asin
+            };
+            var music = new Music(musicDict);
+            mediaList[index] = music;
+            callback();
+        } else if (type === 'Movie'){
+            var movieDict = {
+                title: fields.title,
+                director: fields.director,
+                producers: fields.producers,
+                actors: fields.actors,
+                language: fields.language,
+                subtitles: fields.subtitles,
+                dubbed: fields.dubbed,
+                releaseDate: fields.releaseDate,
+                runTime: fields.runTime
+            };
+            var movie = new Movie(movieDict);
+            mediaList[index] = movie;
+            callback();
+        } else {
+            var err = new Error('Error in type specified.');
+            callback(err);
+        }
+    }
 
     static viewItems() {}
 
     static searchItem(type, fields, callback){
         var err = null;
+        var index = 0;
+
         for (var item of mediaList){
             if (type === 'Book' && item instanceof Book){
                 if (fields.isbn10 === item.isbn10){
-                    callback(err, item);
+                    callback(err, item, index);
                 }
             } else if (type === 'Magazine' && item instanceof Magazine){
                 if (fields.isbn10 === item.isbn10){
-                    callback(err, item);
+                    callback(err, item, index);
                 }
             } else if (type === 'Music' && item instanceof Music){
                 if (fields.asin === item.asin){
-                    callback(err, item);
+                    callback(err, item, index);
                 }
             } else if (type === 'Movie' && item instanceof Movie){
                 if (fields.title === item.title && fields.releaseDate === item.releaseDate){
-                    callback(err, item);
+                    callback(err, item, index);
                 }
             }
+            index++;
         }
-        callback(err, item);
+        callback(err, item, index);
     }
 }
