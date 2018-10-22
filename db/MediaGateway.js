c7f42d43c2bcf90968c34f9d02f0a82ee0ca7551
sbouchard09 <scottbouchard09@gmail.com>
2018-10-21 22:43:30 -0400

    added SQL to MediaGateway functions. #57

--- db/MediaGateway.js
+++ db/MediaGateway.js
@@ -6,30 +6,73 @@ export class MediaGateway extends DatabaseManager {
     static saveMedia(type, fields, callback) {
         this.findMedia(type, fields, (err, rows) => {
             if (err) {
-                throw new Error('Media does not exist');
+                err = new Error('There was an error checking for the item\'s existence');
+                callback(err, rows);
+                return;
             }
-            return rows;
+            if (rows != null) {
+                err = new Error('Media item already exists in the database');
+                callback(err, rows);
+                return;
+            }
+            var query;
+            if (type === 'Book') {
+                query = db.format('INSERT INTO book(title, author, format, pages, publisher, publicationDate, language, isbn10, isbn13) VALUES (?)',
+                    fields);
+                db.query(query, (err, rows, fields) => {
+                    callback(err);
+                });
+            } else if (type === 'Magazine') {
+                query = db.format('INSERT INTO magazine(title, publisher, publicationDate, language, isbn10, isbn13) VALUES (?)',
+                    fields);
+                db.query(query, (err, rows, fields) => {
+                    callback(err);
+                });
+            } else if (type === 'Music') {
+                query = db.format('INSERT INTO music(type, title, artist, label, releaseDate, asin) VALUES (?)',
+                    fields);
+                db.query(query, (err, rows, fields) => {
+                    callback(err);
+                });
+            } else if (type === 'Movie') {
+                query = db.format('INSERT INTO movie(title, director, producers, actors, language, subtitles, dubbed, releaseDate, runtime VALUES (?)',
+                    fields);
+                db.query(query, (err, rows, fields) => {
+                    callback(err);
+                });
+            } else {
+                err = new Error('Error in specified type');
+                callback(err, rows);
+            }
         });
     }
 
     static editMedia(type, fields, callback) {
         this.findMedia(type, fields, (err, rows) => {
             if (err) {
-                throw new Error('Media does not exist');
+                err = new Error('There was an error checking for the item\'s existence');
+                callback(err, rows);
+                return;
             }
-            return rows;
+            if (rows == null) {
+                err = new Error('Media item does not exist in the database');
+                callback(err, rows);
+                return;
+            }
+
+            var query;
+            if (type === 'Book') {
+
+            } else if (type === 'Magazine') {
+
+            } else if (type === 'Music') {
+
+            } else if (type === 'Movie') {
+
+            } else {
+
+            }
         });
-
-        if (type === 'Book') {
-        } else if (type === 'Magazine') {
-
-        } else if (type === 'Music') {
-
-        } else if (type === 'Movie') {
-
-        } else {
-            // error statement will be included once we figure something out for logical stuff.
-        }
     }
 
     static findMedia(type, fields, callback) {
@@ -46,22 +89,34 @@ export class MediaGateway extends DatabaseManager {
     }
 
     static deleteMedia(type, fields, callback) {
-        var mediaTitle = fields['title'];
         this.findMedia(type, fields, (err, rows) => {
             if (err) {
                 throw new Error('Media does not exist');
             }
-            return rows;
-        });
+            var query;
 
-        const SQLQuery = db.format('REMOVE * FROM ? WHERE title=?', [
-            type,
-            mediaTitle
-        ]);
+            if (type === 'Book') {
+                query = db.format('DELETE FROM book WHERE isbn10 = ?',
+                    fields['isbn10']);
+            } else if (type === 'Magazine') {
+                query = db.format('DELETE FROM magazine WHERE isbn10 = ?',
+                    fields['isbn10']);
+            } else if (type === 'Music') {
+                query = db.format('DELETE FROM music WHERE asin = ?',
+                    fields['asin']);
+            } else if (type === 'Movie') {
+                query = db.format('DELETE FROM movie WHERE title = ? AND releaseDate = ?',
+                    [fields['title'], fields['releaseDate']]);
+            }
 
-        db.query(SQLQuery, (err, rows, fields) => {
+            db.query(query, (err, rows, fields) => {
             callback(err);
         });
+        });
+    }
+
+    static getAll(callback) {
+        // insert code that connects the four tablesz
     }
 
     static jsonToMedia(err, jsonArray, fields, callback) {
@@ -74,9 +129,6 @@ export class MediaGateway extends DatabaseManager {
             mediaArray.push(media);
         }
         callback(err, mediaArray);
-    }
-    static getAll(callback) {
-        // insert code that connects the four tablesz
     }
 }
 
