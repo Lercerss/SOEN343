498869d3de1aaca8d494a79bc115e82ac5394ea5
sbouchard09 <scottbouchard09@gmail.com>
2018-10-22 23:21:50 -0400

    Moved calls to findMedia in MediaGateway.js to Catalog.js

--- models/Catalog.js
+++ models/Catalog.js
@@ -3,17 +3,42 @@ import { Magazine } from './Magazine';
 import { Movie } from './Movie';
 import { Music } from './Music';
 import { MediaGateway } from '../db/MediaGateway';
+import { Media } from './Media';
 
 var id = 0;
 var mediaList = new Array();
 
 export class Catalog {
     static addItem(type, fields, callback) {
-        MediaGateway.saveMedia(type, fields, callback);
+        MediaGateway.findMedia(type, fields, (err, rows) => {
+            if (err) {
+                err = new Error('There was an error checking for the item\'s existence');
+                callback(err, rows);
+                return;
+            }
+            if (rows != null) {
+                err = new Error('Media item already exists in the database');
+                callback(err, rows);
+                return;
+            }
+            MediaGateway.saveMedia(type, fields, callback);
+        });
     }
 
     static editItem(type, fields, callback) {
-        MediaGateway.editMedia(type, fields, callback);
+        this.findMedia(type, fields, (err, rows) => {
+            if (err) {
+                err = new Error('There was an error checking for the item\'s existence');
+                callback(err, rows);
+                return;
+            }
+            if (rows == null) {
+                err = new Error('Media item does not exist in the database');
+                callback(err, rows);
+                return;
+            }
+            MediaGateway.editMedia(type, fields, callback);
+        });
     }
 
     static viewItems() {
@@ -38,6 +63,23 @@ export class Catalog {
     }
 
     static deleteItem(type, fields, callback){
-        MediaGateway.deleteMedia(type, fields, callback);
+        this.findMedia(type, fields, (err, rows) => {
+            if (err) {
+                throw new Error('Media does not exist');
+            }
+            MediaGateway.deleteMedia(type, fields, callback);
+        });
+    }
+
+    static jsonToMedia(err, jsonArray, fields, callback) {
+        if (err) {
+            callback(err, []);
+        }
+        var mediaArray = [];
+        for (var mediaJson of jsonArray) {
+            var media = new Media(mediaJson);
+            mediaArray.push(media);
+        }
+        callback(err, mediaArray);
     }
 }
