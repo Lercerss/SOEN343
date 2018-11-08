9279e4a2715b1ff1c957503f5bebee491867ae58
pvacca97 <patrick.vacca@hotmail.com>
2018-11-08 15:46:38 -0500

    fixed typo

--- anansiUI/src/components/ItemsList/index.jsx
+++ anansiUI/src/components/ItemsList/index.jsx
@@ -52,7 +52,7 @@ export default class ItemsList extends React.Component {
     handleEdit = item => {
         this.setState({
             isEditFormShown: true,
-            editeditFormMediaType: item.type,
+            editFormMediaType: item.type,
             itemInfo: item.itemInfo
         });
     };
@@ -74,7 +74,7 @@ export default class ItemsList extends React.Component {
         if (!itemInfo) {
             this.setState({
                 isEditFormShown: false,
-                editeditFormMediaType: ""
+                editFormMediaType: ""
             });
             return;
         }
@@ -86,7 +86,7 @@ export default class ItemsList extends React.Component {
         ].itemInfo = itemInfo;
         this.setState({
             isEditFormShown: false,
-            editeditFormMediaType: "",
+            editFormMediaType: "",
             itemsList: items
         });
         console.log(items);
