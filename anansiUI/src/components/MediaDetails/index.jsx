3adc20b360d91fa2ab3f7e82a457018ba52911ee
pvacca97 <patrick.vacca@hotmail.com>
2018-11-14 12:27:21 -0500

    removed uneeded line

--- anansiUI/src/components/MediaDetails/index.jsx
+++ anansiUI/src/components/MediaDetails/index.jsx
@@ -7,7 +7,6 @@ import MusicDetails from "./MusicDetails";
 export default class MediaDetails extends React.Component {
     render() {
         const { item, visible } = this.props;
-        console.log(this.props);
         let detailComponent = '';
         if (item.type == 'Book') detailComponent = <BookDetails item={item} />;
         else if (item.type == 'Magazine') detailComponent = <MagazineDetails item={item} />;
