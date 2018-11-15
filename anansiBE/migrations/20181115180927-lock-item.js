76c71b9f8a148c8a15748888de6df18f87d2a0da
Riya Dutta <riya.dutta1001@gmail.com>
2018-11-15 16:37:14 -0500

    Issue #121 modified migrations

--- anansiBE/migrations/20181115180927-lock-item.js
+++ anansiBE/migrations/20181115180927-lock-item.js
@@ -15,25 +15,42 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-    return db.addColumn('music', 'isLocked', { type: 'boolean', defaultValue: 0 }, err => {
-        if (err) throw err;
-        db.addColumn('movies', 'isLocked', { type: 'boolean', defaultValue: 0 }, err => {
-            if (err) throw err;
-            db.addColumn('magazines', 'isLocked', { type: 'boolean', defaultValue: 0 }, err => {
-                if (err) throw err;
-                db.addColumn('books', 'isLocked', { type: 'boolean', defaultValue: 0 }, err => {
-                    if (err) throw err;
-                });
-            });
-        });
-    });
+    db.addForeignKey('books', 'users', 'locked_by_id', { 'id': 'client_id' }, {
+        onDelete: 'NO ACTION',
+        onUpdate: 'RESTRICT',
+    }
+    );
+    db.addForeignKey('magazines', 'users', 'locked_by_id', { 'id': 'client_id' }, {
+        onDelete: 'NO ACTION',
+        onUpdate: 'RESTRICT',
+    }
+    );
+    db.addForeignKey('movies', 'users', 'locked_by_id', { 'id': 'client_id' }, {
+        onDelete: 'NO ACTION',
+        onUpdate: 'RESTRICT',
+    }
+    );
+    db.addForeignKey('music', 'users', 'locked_by_id', { 'id': 'client_id' }, {
+        onDelete: 'NO ACTION',
+        onUpdate: 'RESTRICT',
+    }
+    );
+    db.addColumn('books', 'locked_at', { type: 'datetime', defaultValue: null });
+    db.addColumn('magazines', 'locked_at', { type: 'datetime', defaultValue: null });
+    db.addColumn('movies', 'locked_at', { type: 'datetime', defaultValue: null });
+    db.addColumn('music', 'locked_at', { type: 'datetime', defaultValue: null });
+    return null;
 };
 
 exports.down = function(db) {
-    db.removeColumn('music', 'isLocked');
-    db.removeColumn('movies', 'isLocked');
-    db.removeColumn('magazines', 'isLocked');
-    db.removeColumn('books', 'isLocked');
+    db.removeForeignKey('books', 'locked_by_id');
+    db.removeForeignKey('magazines', 'locked_by_id');
+    db.removeForeignKey('movies', 'locked_by_id');
+    db.removeForeignKey('music', 'locked_by_id');
+    db.removeColumn('music', 'locked_at');
+    db.removeColumn('movies', 'locked_at');
+    db.removeColumn('magazines', 'locked_at');
+    db.removeColumn('books', 'locked_at');
     return null;
 };
 
