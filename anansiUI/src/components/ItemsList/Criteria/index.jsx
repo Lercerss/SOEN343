620f1db3e75931f20bd862e2513ffda5663de758
Lucas Turpin <lucas_turpin@hotmail.com>
2018-11-18 20:12:39 -0500

    Reset shown details view when item list changes #107

--- anansiUI/src/components/ItemsList/Criteria/index.jsx
+++ anansiUI/src/components/ItemsList/Criteria/index.jsx
@@ -127,11 +127,11 @@ class Criteria extends React.Component {
         const formItemLayout = {
             labelCol: {
                 xs: { span: 24 },
-                sm: { span: 8 }
+                sm: { span: 4 }
             },
             wrapperCol: {
                 xs: { span: 24 },
-                sm: { span: 16 }
+                sm: { span: 20 }
             }
         };
         getFieldDecorator('keys', { initialValue: [] });
